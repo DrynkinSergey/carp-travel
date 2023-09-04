09b2bdfe4d80fde5bd4c2f71371c38b155b78b95
Sergey <sergsmile29@gmail.com>
2023-09-05 02:54:29 +0300

    swiper gallary

--- components/OurGallary.jsx
+++ components/OurGallary.jsx
@@ -1,7 +1,9 @@
+'use client'
 import Image from 'next/image'
 import React from 'react'
 import { Title } from './Title'
-
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
 const OurGallary = () => {
 	return (
 		<section
@@ -9,9 +11,34 @@ const OurGallary = () => {
 			className='px-5  py-10 flex text-white font-thin flex-col gap-6 items-center  bg-cover  '
 		>
 			<Title onePart='our' secondPart='GALLERY' />
-			<Image src='/img1.png' alt='img1' width={300} height={100} />
-			<Image src='/img2.png' alt='img2' width={300} height={100} />
-			<Image src='/img3.png' alt='img3' width={300} height={100} />
+			<section className='md:hidden'>
+				<Image src='/img1.png' alt='img1' width={300} height={100} />
+				<Image src='/img2.png' alt='img2' width={300} height={100} />
+				<Image src='/img3.png' alt='img3' width={300} height={100} />
+			</section>
+
+			<div className='w-2/3 overflow-hidden'>
+				<Swiper slidesPerView={3} loop={true}>
+					<SwiperSlide>
+						<Image src='/img1.png' alt='img1' width={300} height={100} />
+					</SwiperSlide>
+					<SwiperSlide>
+						<Image src='/img2.png' alt='img2' width={300} height={100} />
+					</SwiperSlide>
+					<SwiperSlide>
+						<Image src='/img3.png' alt='img3' width={300} height={100} />
+					</SwiperSlide>
+					<SwiperSlide>
+						<Image src='/img1.png' alt='img1' width={300} height={100} />
+					</SwiperSlide>
+					<SwiperSlide>
+						<Image src='/img2.png' alt='img2' width={300} height={100} />
+					</SwiperSlide>
+					<SwiperSlide>
+						<Image src='/img3.png' alt='img3' width={300} height={100} />
+					</SwiperSlide>
+				</Swiper>
+			</div>
 		</section>
 	)
 }
