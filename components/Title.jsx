c2707e24e99750c795fbbc28af6db1e050e5e728
Sergey <sergsmile29@gmail.com>
2023-09-05 00:41:04 +0300

    who we are adaptive

--- components/Title.jsx
+++ components/Title.jsx
@@ -1,8 +1,9 @@
+import clsx from 'clsx'
 import React from 'react'
 
-export const Title = ({ onePart, secondPart }) => {
+export const Title = ({ onePart, secondPart, className }) => {
 	return (
-		<h1 className='uppercase text-4xl'>
+		<h1 className={clsx('uppercase text-4xl', className && className)}>
 			<span>{onePart}</span> <span className='font-bold'>{secondPart}</span>
 		</h1>
 	)
--- components/WhoWeAre.jsx
+++ components/WhoWeAre.jsx
@@ -3,34 +3,72 @@ import { Title } from './Title'
 
 const WhoWeAre = () => {
 	return (
-		<section
-			style={{ background: 'url("/background_two.png") no-repeat 20%/cover' }}
-			className='px-5 py-10 flex text-white font-thin flex-col gap-6  bg-cover  '
-		>
-			<Title onePart='WHO' secondPart='WE ARE' />
-			<p className='w-3/4'>
-				<span className='font-light'>a team of enthusiasts</span> who are fully committed to the mission of creating
-				unforgettable and extraordinary trips to the most beautiful parts of the Carpathians. Our goal is not just to
-				show you the natural wonders of the mountains, but to provide you with a deep immersion in their magical
-				atmosphere.
-			</p>
-			<p className='w-3/4'>
-				<span className='font-light'>We believe</span> that nature has the power to inspire, strengthen character and
-				provide new perspectives. Therefore, each of our tours is aimed at unlocking your potential, enriching your
-				spiritual world and creating unforgettable memories.
-			</p>
+		<>
+			<section
+				style={{ background: 'url("/background_two.png") no-repeat 20%/cover' }}
+				className='md:hidden px-5 py-10 flex text-white font-thin flex-col gap-6  bg-cover  '
+			>
+				<Title onePart='WHO' secondPart='WE ARE' />
+				<p className='w-3/4'>
+					<span className='font-light'>a team of enthusiasts</span> who are fully committed to the mission of creating
+					unforgettable and extraordinary trips to the most beautiful parts of the Carpathians. Our goal is not just to
+					show you the natural wonders of the mountains, but to provide you with a deep immersion in their magical
+					atmosphere.
+				</p>
+				<p className='w-3/4'>
+					<span className='font-light'>We believe</span> that nature has the power to inspire, strengthen character and
+					provide new perspectives. Therefore, each of our tours is aimed at unlocking your potential, enriching your
+					spiritual world and creating unforgettable memories.
+				</p>
 
-			<div className='flex flex-col text-right'>
-				<p className='uppercase font-light pr-10'>From vacationers </p>
-				<p className='uppercase font-light'>to active travelers </p>
-				<p className='text-base'>we have a tour for everyone. </p>
-			</div>
+				<div className='flex flex-col text-right'>
+					<p className='uppercase font-light pr-10'>From vacationers </p>
+					<p className='uppercase font-light'>to active travelers </p>
+					<p className='text-base'>we have a tour for everyone. </p>
+				</div>
 
-			<p className='w-full'>
-				<span className='font-light'>We use methods</span> that are time-tested and proven. Our expert guides with
-				in-depth knowledge of the Carpathian landscapes lead you safely through the mysteries of these mountains.
-			</p>
-		</section>
+				<p className='w-full'>
+					<span className='font-light'>We use methods</span> that are time-tested and proven. Our expert guides with
+					in-depth knowledge of the Carpathian landscapes lead you safely through the mysteries of these mountains.
+				</p>
+			</section>
+
+			{/* Tablet */}
+			<section
+				style={{ background: 'url("/background_two.png") no-repeat 20%/cover' }}
+				className=' md:grid   px-5 py-10 hidden text-white font-thin  gap-6  bg-cover  '
+			>
+				<div className='grid-cols-[3fr,2fr]  justify-center grid mb-10 '>
+					<div className='flex flex-col  justify-between '>
+						<Title onePart='WHO' secondPart='WE ARE' className='text-6xl ' />
+						<div className='flex flex-col '>
+							<p className='uppercase font-light '>From vacationers </p>
+							<p className='uppercase font-light pl-10'>to active travelers </p>
+							<p className='text-base'>we have a tour for everyone. </p>
+						</div>
+					</div>
+					<div className=''>
+						<p>
+							<span className='font-light'>a team of enthusiasts</span> who are fully committed to the mission of
+							creating unforgettable and extraordinary trips to the most beautiful parts of the Carpathians. Our goal is
+							not just to show you the natural wonders of the mountains, but to provide you with a deep immersion in
+							their magical atmosphere.
+						</p>
+						<p className='w-3/4'>
+							<span className='font-light'>We believe</span> that nature has the power to inspire, strengthen character
+							and provide new perspectives. Therefore, each of our tours is aimed at unlocking your potential, enriching
+							your spiritual world and creating unforgettable memories.
+						</p>
+					</div>
+				</div>
+				<div className='flex justify-end'>
+					<p className='w-2/3'>
+						<span className='font-light'>We use methods</span> that are time-tested and proven. Our expert guides with
+						in-depth knowledge of the Carpathian landscapes lead you safely through the mysteries of these mountains.
+					</p>
+				</div>
+			</section>
+		</>
 	)
 }
 
