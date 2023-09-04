f0c6f9a0ab47415b8f8705cff290d6adac8d8c9e
Sergey <sergsmile29@gmail.com>
2023-09-05 02:23:07 +0300

    chooseUs

--- components/ChooseUs.jsx
+++ components/ChooseUs.jsx
@@ -9,39 +9,53 @@ const ChooseUs = () => {
 				style={{ background: 'url("/images/chooseUsBg.png") no-repeat 20%/cover' }}
 				className='px-5  py-10 flex text-white font-thin flex-col gap-6  bg-cover  '
 			>
-				<Title onePart='choose' secondPart='us' />
-				<div className='flex justify-end px-2'>
-					<p className='flex justify-right py-6 w-2/3'>
-						Your chance to join our passionate team in Carpathian tourism. Seeking talented professionals to share our
-						common mission.
-					</p>
+				<section className='grid md:grid-cols-[2fr,1fr] items-center'>
+					<Title onePart='choose' secondPart='us' className='md:text-6xl' />
+					<div className='flex justify-end px-2'>
+						<p className='flex justify-right py-6 w-2/3 md:w-full md:text-sm'>
+							Your chance to join our passionate team in Carpathian tourism. Seeking talented professionals to share our
+							common mission.
+						</p>
+					</div>
+				</section>
+
+				<h2 className='uppercase font-extralight py-2 text-right md:text-left md:ml-20 mr-10 text-3xl'>Why us ?</h2>
+
+				<div className='grid md:grid-cols-[1fr,2fr] gap-5 '>
+					<ul className='grid gap-4 text-right w-2/3 md:w-full '>
+						<li>
+							<h3 className='font-normal text-sm mb-2'>Professional development</h3>
+							<p>We offer growth opportunities and a creative environment to nurture your talents.</p>
+						</li>
+						<li>
+							<h3 className='font-normal text-sm mb-2'>Teamwork</h3>
+							<p>Join our close-knit family, where support and inspiration abound.</p>
+						</li>
+						<li>
+							<h3 className='font-normal text-sm mb-2'>Stimulating work environment</h3>
+							<p>Flexibility and remote options for a comfortable experience.</p>
+						</li>
+						<li>
+							<h3 className='font-normal text-sm mb-2'>Exciting challenges</h3>
+							<p>Unleash your potential through unforgettable projects showcasing Carpathian beauty.</p>
+						</li>
+					</ul>
+					<section className='md:block hidden'>
+						<div className='flex justify-end md:justify-start px-2'>
+							<p className='flex  justify-right py-6 w-2/3 md:w-1/2 '>
+								Don't miss your opportunity! Fill out the form right now and join our team!
+							</p>
+						</div>
+						<ChooseUsForm />
+					</section>
 				</div>
-				<h2 className='uppercase font-extralight py-2 text-right mr-10 text-3xl'>Why us ?</h2>
-				<ul className='flex flex-col gap-4 text-right w-2/3'>
-					<li>
-						<h3 className='font-normal text-sm mb-2'>Professional development</h3>
-						<p>We offer growth opportunities and a creative environment to nurture your talents.</p>
-					</li>
-					<li>
-						<h3 className='font-normal text-sm mb-2'>Teamwork</h3>
-						<p>Join our close-knit family, where support and inspiration abound.</p>
-					</li>
-					<li>
-						<h3 className='font-normal text-sm mb-2'>Stimulating work environment</h3>
-						<p>Flexibility and remote options for a comfortable experience.</p>
-					</li>
-					<li>
-						<h3 className='font-normal text-sm mb-2'>Exciting challenges</h3>
-						<p>Unleash your potential through unforgettable projects showcasing Carpathian beauty.</p>
-					</li>
-				</ul>
 			</section>
 			<section
 				style={{ background: 'url("/images/chooseUsBg.png") no-repeat 20%/cover' }}
-				className='px-5  py-10 flex text-white font-thin flex-col gap-6  bg-cover  '
+				className='px-5 md:hidden py-10 flex text-white font-thin flex-col gap-6  bg-cover  '
 			>
 				<div className='flex justify-end px-2'>
-					<p className='flex justify-right py-6 w-2/3'>
+					<p className='flex  py-6 w-2/3 '>
 						Don't miss your opportunity! Fill out the form right now and join our team!
 					</p>
 				</div>
--- components/ChooseUsForm.jsx
+++ components/ChooseUsForm.jsx
@@ -12,58 +12,62 @@ const ChooseUsForm = () => {
 		console.log('success')
 	}
 	return (
-		<form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-4'>
-			<div className='flex flex-col gap-2'>
-				<label>Full name</label>
-				<input
-					{...register('name', { required: true, minLength: 4 })}
-					className='bg-white/[0.05] px-4'
-					placeholder='John Smith'
-				/>
-				{errors.name && <div className='text-red-400 font-light text-right'>x Incorrect name</div>}
-			</div>
-			<div className='flex flex-col gap-2'>
-				<label>E-mail</label>
-				<input
-					{...register('email', { required: true, pattern: /^\S+@\S+$/i })}
-					className='bg-white/[0.05] px-4'
-					placeholder='johnsmith@email.com'
-				/>
-				{errors.email && <div className='text-red-400 font-light text-right'>x Invalid email</div>}
-			</div>
+		<form onSubmit={handleSubmit(onSubmit)} className='grid md:grid-cols-2 gap-4'>
+			<div>
+				<div className='flex flex-col gap-2'>
+					<label>Full name</label>
+					<input
+						{...register('name', { required: true, minLength: 4 })}
+						className='bg-white/[0.05] px-4'
+						placeholder='John Smith'
+					/>
+					{errors.name && <div className='text-red-400 font-light text-right'>x Incorrect name</div>}
+				</div>
+				<div className='flex flex-col gap-2'>
+					<label>E-mail</label>
+					<input
+						{...register('email', { required: true, pattern: /^\S+@\S+$/i })}
+						className='bg-white/[0.05] px-4'
+						placeholder='johnsmith@email.com'
+					/>
+					{errors.email && <div className='text-red-400 font-light text-right'>x Invalid email</div>}
+				</div>
 
-			<div className='flex flex-col gap-2'>
-				<label>Position</label>
-				<input
-					{...register('position', { required: true, minLength: 2 })}
-					className='bg-white/[0.05] px-4'
-					placeholder='Movie maker'
-				/>
-				{errors.position && <div className='text-red-400 font-light text-right'>x Invalid field</div>}
-			</div>
+				<div className='flex flex-col gap-2'>
+					<label>Position</label>
+					<input
+						{...register('position', { required: true, minLength: 2 })}
+						className='bg-white/[0.05] px-4'
+						placeholder='Movie maker'
+					/>
+					{errors.position && <div className='text-red-400 font-light text-right'>x Invalid field</div>}
+				</div>
 
-			<div className='flex flex-col gap-2'>
-				<label>Phone</label>
-				<input
-					{...register('phone', { required: true })}
-					className='bg-white/[0.05] px-4'
-					placeholder='+38 (097) 12 34 567'
-				/>
-				{errors.position && <div className='text-red-400 font-light text-right'>x Invalid field</div>}
+				<div className='flex flex-col gap-2'>
+					<label>Phone</label>
+					<input
+						{...register('phone', { required: true })}
+						className='bg-white/[0.05] px-4'
+						placeholder='+38 (097) 12 34 567'
+					/>
+					{errors.position && <div className='text-red-400 font-light text-right'>x Invalid field</div>}
+				</div>
 			</div>
+			{/* Message  */}
 
 			<div className='flex flex-col gap-2'>
 				<label>Message</label>
 				<textarea className='h-[200px] resize-none bg-white/[0.05] px-4' type='email' />
 			</div>
-
-			<div className='flex items-center gap-4  '>
+			{/* Confirm  */}
+			<div className=' flex items-center gap-4  '>
 				<div className='border-[1px] border-white px-[2px] flex items-center justify-center py-[1px] w-8 h-6 mx-2'>
 					<input type='checkbox' className='w-full h-full' />
 				</div>
 				<label className='text-xs'>I confirm my consent to the processing of personal data.</label>
 			</div>
-			<div className='flex justify-end'>
+			{/* Send button */}
+			<div className=' flex justify-end'>
 				<button className=' bg-none font-medium text-3xl uppercase  '>Send</button>
 			</div>
 		</form>
